01151630453b6e7b6868ac1f6c44f5cec5985711
naoto <stardustforever012@gmail.com>
2021-12-13 00:11:16 +0900

    fix: 🐛 fix typo and failed test

--- src/notarealdb-core.module.ts
+++ src/notarealdb-core.module.ts
@@ -35,7 +35,7 @@ export class NotARealDbCoreModule {
   private static createConnection(options: NotARealDbOptions): DataStore {
     const pwd =
       process.env.NODE_ENV === 'test'
-        ? `${process.cwd()}/tests/app/`
+        ? `${process.cwd()}/test/app/`
         : process.cwd();
     const dirPath = path.resolve(
       pwd,
--- test/app/apples/dto/create-apple.dto.ts
+++ test/app/apples/dto/create-apple.dto.ts
@@ -1,7 +1,6 @@
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateAppleDto {
-
   @IsString()
   variety: string;
 
